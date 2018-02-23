398d2edd53ab2cff4e77c86f84a2565fd622d9ec
you <you@eggsample.com>
2018-02-23 11:47:55 +0200

    first commit

--- test1.py
+++ test1.py
@@ -12,3 +12,13 @@ a,b,c,d = 3,5.7,'tere',True
 
 #konstant
 PI = 3.14
+true = True
+false = False
+d = true
+
+x=0.1+0.1+0.1-0.3 #vastus vigane
+
+sX = "string \"abc\""
+sY = "see on \\ jagamismark"
+
+sZ = '-' * 20
