67bc6802695825c39db1eb2685cea2d701132cbb
sander-h <36733019+sander-h@users.noreply.github.com>
2018-04-27 17:57:29 +0300

    Update 02.py

--- assignments/02.py
+++ assignments/02.py
@@ -6,7 +6,7 @@ def f_printList(list=3*[3*["-"]]):
     yPos = 0
     for xList in mainList:
         if yPos == 1:
-            print()
+            print("--+-+-+")
         print (str(yPos)+" |",end='')
         yPos += 1
         for x in xList:
@@ -14,7 +14,7 @@ def f_printList(list=3*[3*["-"]]):
         print("")
 f_printList()
 
-#get input for player
+# return player input for single number
 def f_getPlayerInput(hint="Sisesta Number (1/2/3)",hintError="Vigane Number v6i number ei sobi, proovi uuesti",range=[1,2,3]):
     while True:
         print(hint)
@@ -23,17 +23,66 @@ def f_getPlayerInput(hint="Sisesta Number (1/2/3)",hintError="Vigane Number v6i 
             return i
         else:
             print(hintError)
+
+# returns true if position is empty
 def f_canUpdateList(list,xPos=1,yPos=1):
     xList = list[xPos-1]
     return (xList[yPos-1] == "-")
-def f_updateList(list,offSet=-1,xPos=1,yPos=1,newVal='-')
-    list[xPos+offSet][yPos+offSet] = newVal
-    return list
-def f_getPlayerPos(player="x"):
-    while True:
-        xPos = f_getPlayerInput(hint='X, vali positsioon x teljel')
-        yPos = f_getPlayerInput(hint='X, vali positsioon y teljel')
 
+# returns new list with updated value for certain square
+def f_updateList(list,posList=[1,1],newVal='-'):
+    xPos = posList[0]
+    yPos = posList[1]
+    list[xPos-1][yPos-1] = newVal
+    return list
+
+# loop until player has selected a empty position
+def f_getPlayerPos(list,player="x"):
+    while True:
+        xPos = f_getPlayerInput(hint=player+', vali positsioon x teljel')
+        yPos = f_getPlayerInput(hint=player+', vali positsioon y teljel')
+        if f_canUpdateList(list):
+            return [xPos,yPos]
+        else:
+            print("Positsioon :"+ str(xPos)+", "+ str(yPos)+" ei sobi")
+
+def f_checkWin(list):
+    winner = None
+    for player in ["x","y"]:
+        score = 0
+        d1Score = 0
+        d2score = 0
+        y1Score = 0
+        y2Score = 0
+        y3Score = 0
+        for y in range(0,2):
+            xList = list[y]
+            xScore = 0
+            for x in range(0,2):
+                xVal = xList[x]
+                if xVal == player:
+                    xScore += 1
+                    if x == 0:
+                        y1Score += 1
+                        if y == 0:
+                            d1Score += 1
+                        if y == 2:
+                            d2score += 1
+                    if x == 1:
+                        y2Score += 1
+                        if y == 1:
+                            d1Score += 1
+                            d2Score += 1
+                    if x == 2:
+                        y3Score += 1
+                        if y == 2:
+                            d1Score += 1
+                        if y == 0:
+                            d2Score += 1
+            if xScore == 3:
+                return player
+
+                
 
 def f_play():
     tripsList = 3*[3*["-"]]
@@ -47,7 +96,12 @@ def f_play():
         if end != None:
             print(end)
             break
+        tripsList = f_updateList(tripsList,f_getPlayerPos(tripsList,player))
+        f_printList(tripsList)
         if player == 'x':
+            player = 'y'
+        else:
+            player = 'x'
 
         
         
